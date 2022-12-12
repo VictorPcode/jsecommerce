bb2d01496f87e9da710551920d636fcc26843d0e
VictorPcode <116080710+VictorPcode@users.noreply.github.com>
2022-12-12 12:32:45 -0300

    reestablecida las conexiones en una version mas reciente de mssql y django

--- jsecommerce/settings.py
+++ jsecommerce/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from django.conf import settings
-from django.conf.urls.static import static
+
 from pathlib import Path
-import os
+
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,12 +85,12 @@ DATABASES = {
         "USER": 'JSPROFILE',
         "PASSWORD": 'seguridad2022**',
         "HOST": 'EMAUXBACKEND',
-        "PORT": '1433',
-        "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server",
+        "PORT": '',
+        "OPTIONS": {
                     },
+        
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -126,11 +128,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static'
-STATICFILES_DIRS = [
-   
-    '/static/css',
-    '/static/image',
-                    ]
+STATICFILES_DIRS = (
+    'static/css/',
+    )
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
