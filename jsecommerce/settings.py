63f3c3fc6043d15d8976d8741ad415839138ea79
VictorPcode <116080710+VictorPcode@users.noreply.github.com>
2023-04-28 08:36:04 -0400

    commit de respaldo jscode

--- jsecommerce/settings.py
+++ jsecommerce/settings.py
@@ -131,7 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR/'static'
+
 STATICFILES_DIRS = [
     'jsecommerce/static',
     'static/css'
