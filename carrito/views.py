555aa9d98f81d29e2368d3cb92ab446c2a896260
VictorPcode <116080710+VictorPcode@users.noreply.github.com>
2023-05-01 01:16:35 -0400

    Desarrollo (#1)
    * avances en el carrito, suma los productos, elimina o resta. Muestra el sub total suma con el iva y da el total general.

--- carrito/views.py
+++ carrito/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from tienda.models import Producto
-from  .models import Carrito, CarritoItem
+from .models import Carrito, CarritoItem
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 def _carrito_id(request):
@@ -16,23 +18,74 @@ def agregarCarrito(request, product_id):
         carrito = Carrito.objects.get(carrito_id=_carrito_id(request))
     except Carrito.DoesNotExist:
         carrito = Carrito.objects.create(
-            carrito_id = _carrito_id(request)
+            carrito_id=_carrito_id(request)
         )
         carrito.save()
-        
-        try:
-            carrito_item = CarritoItem.objects.get(product=product, carrito=carrito)
-            carrito_item.cantidad +=1
-            carrito_item.save()
-        except CarritoItem.DoesNotExist:
-            carrito_item = CarritoItem.objects.create(
-                product = product,
-                cantidad = 1,
-                carrito = carrito
-            )
-            carrito_item.save
-            
-        return redirect('carrito')
-            
-def carrito(request):
-    return render(request, 'carrito.html')
+
+    try:
+        carrito_item = CarritoItem.objects.get(
+            product=product, carrito=carrito)
+        carrito_item.cantidad += 1
+        carrito_item.save()
+    except CarritoItem.DoesNotExist:
+        carrito_item = CarritoItem.objects.create(
+            product=product,
+            cantidad=1,
+            carrito=carrito
+        )
+        carrito_item.save()
+
+    return redirect('carrito')
+
+
+def remover_carritoItem(request, product_id):
+    carrito = Carrito.objects.get(carrito_id=_carrito_id(request))
+    product = get_object_or_404(Producto, id=product_id)
+    carritoItem = CarritoItem.objects.get(product=product, carrito=carrito)
+
+    if carritoItem.cantidad > 1:
+        carritoItem.cantidad -= 1
+        carritoItem.save()
+    else:
+     carritoItem.delete()
+
+    return redirect('carrito')
+
+
+def carrito(request, total=0, cantidad=0, carritoItem=None):
+
+    try:
+        carrito = Carrito.objects.get(carrito_id=_carrito_id(request))
+        carritoItem = CarritoItem.objects.filter(
+            carrito=carrito, is_active=True)
+        for item in carritoItem:
+            total += (item.product.precio * item.cantidad)
+            cantidad += item.cantidad
+
+        iva = (2*total)/100
+        grand_total = total + iva
+
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'cantidad': cantidad,
+        'carritoItems': carritoItem,
+        'iva': iva,
+        'grand_total': grand_total,
+    }
+
+    return render(request, 'carrito.html', context)
+
+
+def eliminar_del_carrito(request, producto_slug):
+    carrito = Carrito.objects.get(carrito_id=_carrito_id(request))
+    producto = get_object_or_404(Producto, producto_slug=producto_slug)
+    try:
+        carrito_item = CarritoItem.objects.get(
+            product=producto, carrito=carrito)
+        carrito_item.delete()
+    except:
+        pass
+    return redirect('carrito')
--- jsecommerce/settings.py
+++ jsecommerce/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR/'static'
+
 STATICFILES_DIRS = [
     'static/css',
     'static/images',
