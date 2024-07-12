b88341eb6a978ccfa1f9488038d0a262a9c26099
VictorPcode <victorperozocode@gmail.com>
2024-07-12 16:36:21 -0400

    volviendo a la vida

--- jsecommerce/urls.py
+++ jsecommerce/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+print(admin)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogo/', include('catalogo.urls')),
